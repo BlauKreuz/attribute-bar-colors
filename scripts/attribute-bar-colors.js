da3452a5245bda8832fa37abf2d74351bb0c0dbf
BlauKreuz <96351604+BlauKreuz@users.noreply.github.com>
2022-04-29 17:59:51 +0300

    Add files via upload

--- scripts/attribute-bar-colors.js
+++ scripts/attribute-bar-colors.js
@@ -1,7 +1,16 @@
 const MODULE_ID = 'attribute-bar-colors';
 
-function  _drawCBar_Override(number, bar, data) {
-    const val = Number(data.value);
+    function drawBars_Override() {	
+    if ( !this.actor || (this.data.displayBars === CONST.TOKEN_DISPLAY_MODES.NONE) ) return;
+    ["bar1", "bar2"].forEach((b, i) => {
+      const bar = this.hud.bars[b];
+      const attr = this.document.getBarAttribute(b);
+      if ( !attr || (attr.type !== "bar") ) return bar.visible = false;
+	  	  
+	  let number = i;
+	  let data = attr;
+	    	  
+	const val = Number(data.value);
     const pct = Math.clamped(val, 0, data.max) / data.max;
 
     // Determine sizing
@@ -13,15 +22,33 @@ function  _drawCBar_Override(number, bar, data) {
     // Determine the color to use
     const blk = 0x000000;
 
-// Change health bar to red for Shadowrun 5e
-    let prs;
-    if ( number === 0 ) prs = 1 - pct;
-    else prs = pct;
-    let color;
-    if ( number === 0 ) color = PIXI.utils.rgb2hex([(1-(prs/2)), prs, 0]);
-    else color = PIXI.utils.rgb2hex([(0.5 * pct), (0.7 * pct), 0.5 + (pct / 2)]);
-// Change end
+	// Convert color picker hex strings to RGB values
+	const bar1e = hexToRGB((game.settings.get(MODULE_ID, 'bar1empty')).replace('#', '0x').slice(0,8));
+	const bar1f = hexToRGB((game.settings.get(MODULE_ID, 'bar1full')).replace('#', '0x').slice(0,8));
+	const bar2e = hexToRGB((game.settings.get(MODULE_ID, 'bar2empty')).replace('#', '0x').slice(0,8));
+	const bar2f = hexToRGB((game.settings.get(MODULE_ID, 'bar2full')).replace('#', '0x').slice(0,8));
 
+	// Calculate current colors based on attribute bar status
+	const R1= bar1e[0] + ((pct) *(bar1f[0] - bar1e[0]));
+	const G1= bar1e[1] + ((pct) *(bar1f[1] - bar1e[1]));
+	const B1= bar1e[2] + ((pct) *(bar1f[2] - bar1e[2]));
+	
+	const R2= bar2e[0] + ((pct) *(bar2f[0] - bar2e[0]));
+	const G2= bar2e[1] + ((pct) *(bar2f[1] - bar2e[1]));
+	const B2= bar2e[2] + ((pct) *(bar2f[2] - bar2e[2]));	
+	
+    // Change health bar to red for Shadowrun 5e
+    let color;		
+ /*
+	if ( number === 0 ) color = PIXI.utils.rgb2hex([0.5 + (0.5 * pct), (0.2 * pct), (0.2 * pct)]);
+    else color = PIXI.utils.rgb2hex([(0.3 * pct), (0.3 * pct), 0.5 + (0.5 * pct)]);
+*/	
+	if ( number === 0 ) color = PIXI.utils.rgb2hex([R1,G1,B1]);
+    else color = PIXI.utils.rgb2hex([R2,G2,B2]);
+	
+console.log("E: ", bar1e[0]*255, bar1e[1]*255, bar1e[2]*255);
+console.log("F: ", bar1f[0]*255, bar1f[1]*255, bar1f[2]*255);
+console.log("RGB: ",R1*255, G1*255, B1*255);
     // Draw the bar
     bar.clear()
     bar.beginFill(blk, 0.5).lineStyle(bs, blk, 1.0).drawRoundedRect(0, 0, this.w, h, 3)
@@ -30,27 +57,75 @@ function  _drawCBar_Override(number, bar, data) {
     // Set position
     let posY = number === 0 ? this.h - h : 0;
     bar.position.set(0, posY);
-  }
   
-  
-    function _drawBars_Override() {
-    if ( !this.actor || (this.data.displayBars === CONST.TOKEN_DISPLAY_MODES.NONE) ) return;
-    ["bar1", "bar2"].forEach((b, i) => {
-      const bar = this.hud.bars[b];
-      const attr = this.document.getBarAttribute(b);
-      if ( !attr || (attr.type !== "bar") ) return bar.visible = false;
-      this._drawCBar(i, bar, attr);
       bar.visible = true;
     });
   }
   
-  
-  
-  
-  
+
+
+Hooks.once('init', function () {
+
+
+	new window.Ardittristan.ColorSetting(MODULE_ID, 'bar2empty', {
+    name: "Token Bar 2 (above)",
+	label: "Empty Bar",
+    restricted: true,                  // Restrict to gamemaster only?
+    defaultColor: "#000095ff",          // The default color of the setting
+    scope: "world",                    // The scope of the setting
+    config: true,
+	onChange: (value) => {},            // A callback function
+	})
+
+	new window.Ardittristan.ColorSetting(MODULE_ID, 'bar2full', {
+    name: "Token Bar 2 (above)",
+	label: "Full Bar",	
+    restricted: true,                  // Restrict to gamemaster only?
+    defaultColor: "#7d5accff",          // The default color of the setting
+    scope: "world",                    // The scope of the setting
+    config: true,
+	onChange: (value) => {},            // A callback function
+	})
+
+	new window.Ardittristan.ColorSetting(MODULE_ID, 'bar1empty', {
+    name: "Token Bar 1 (below)",  
+	label: "Empty Bar",	
+    restricted: true,                  // Restrict to gamemaster only?
+    defaultColor: "#7f5522ff",          // The default color of the setting
+    scope: "world",                    // The scope of the setting
+    config: true,
+	onChange: (value) => {},            // A callback function
+	})
+
+	new window.Ardittristan.ColorSetting(MODULE_ID, 'bar1full', {
+    name: "Token Bar 1 (below)",
+	hint: "Each bar color will be calculated based on current status between 0 and max value of the attribute. Scene reload is needed for the changes to take effect!", 
+	label: "Full Bar",
+	restricted: true,                  // Restrict to gamemaster only?
+    defaultColor: "#a50000ff",          // The default color of the setting
+    scope: "world",                    // The scope of the setting
+    config: true,
+	onChange: (value) => {},            // A callback function
+	})
+
+
+
+})
+
+
+
   Hooks.once('setup', function () {
 
-    libWrapper.register(MODULE_ID, 'Token.prototype._drawBars', _drawBars_Override, "OVERRIDE")
+    libWrapper.register(MODULE_ID, 'Token.prototype.drawBars', drawBars_Override, "OVERRIDE")
 
     console.log(`Attribute Bar Colors v1.0 | initialized`)
+	
+
 })
+
+
+Hooks.once('ready', () => {
+    try{window.Ardittristan.ColorSetting.tester} catch {
+        ui.notifications.notify('Please make sure you have the "lib - ColorSettings" module installed and enabled.', "error");
+    }
+});
