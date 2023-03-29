c26b700febd9309a61f5ae8364e4a96f95c9332b
BlauKreuz <96351604+BlauKreuz@users.noreply.github.com>
2023-03-29 18:27:38 +0300

    Update attribute-bar-colors.js

--- scripts/attribute-bar-colors.js
+++ scripts/attribute-bar-colors.js
@@ -2,7 +2,9 @@ const MODULE_ID = 'attribute-bar-colors';
 
 
 function drawBars_Override() {	
-    if ( !this.actor || (this.data.displayBars === CONST.TOKEN_DISPLAY_MODES.NONE) ) return;
+    if ( !this.actor || (this.document.displayBars === CONST.TOKEN_DISPLAY_MODES.NONE) ) {
+		return this.bars.visible = false;
+	}
     ["bar1", "bar2"].forEach((b, i) => {
 		
 		if (this.hud?.bars || this.bars)
@@ -21,16 +23,18 @@ function drawBars_Override() {
     let h = Math.max((canvas.dimensions.size / 12), 8);
     const w = this.w;
     const bs = Math.clamped(h / 8, 1, 2);
-    if ( this.data.height >= 2 ) h *= 1.6;  // Enlarge the bar for large tokens
+    if ( this.document.height >= 2 ) h *= 1.6;  // Enlarge the bar for large tokens
    
     // Determine the color to use
     const blk = 0x000000;
 
 	// Convert color picker hex strings to RGB values
-	const bar1e = hexToRGB((game.settings.get(MODULE_ID, 'bar1empty')).replace('#', '0x').slice(0,8));
-	const bar1f = hexToRGB((game.settings.get(MODULE_ID, 'bar1full')).replace('#', '0x').slice(0,8));
-	const bar2e = hexToRGB((game.settings.get(MODULE_ID, 'bar2empty')).replace('#', '0x').slice(0,8));
-	const bar2f = hexToRGB((game.settings.get(MODULE_ID, 'bar2full')).replace('#', '0x').slice(0,8));
+	//const bar1e = hexToRGB((game.settings.get(MODULE_ID, 'bar1empty')).replace('#', '0x').slice(0,8)); // old definition
+	
+	const bar1e = Color.fromString((game.settings.get(MODULE_ID, 'bar1empty')).replace('#', '0x').slice(0,8)).rgb;	
+	const bar1f = Color.fromString((game.settings.get(MODULE_ID, 'bar1full')).replace('#', '0x').slice(0,8)).rgb;
+	const bar2e = Color.fromString((game.settings.get(MODULE_ID, 'bar2empty')).replace('#', '0x').slice(0,8)).rgb;
+	const bar2f = Color.fromString((game.settings.get(MODULE_ID, 'bar2full')).replace('#', '0x').slice(0,8)).rgb;
 
 	// Calculate current colors based on attribute bar status
 	const R1= bar1e[0] + ((pct) *(bar1f[0] - bar1e[0]));
@@ -50,7 +54,7 @@ function drawBars_Override() {
 	const barScale = (game.settings.get(MODULE_ID, 'bar-scale')); 
 	const barH = h * barScale;
 
-    	if ( this.data.height < 1 ) h *= (0.8 * barScale ) ;  // Decrease the bar for small tokens
+    	if ( this.document.height < 1 ) h *= (0.8 * barScale ) ;  // Decrease the bar for small tokens
 
 	const barBorderAlpha = (game.settings.get(MODULE_ID, 'bar-border-alpha'));
 	const barBackgroundAlpha = (game.settings.get(MODULE_ID, 'bar-background-alpha')); 
@@ -67,40 +71,55 @@ function drawBars_Override() {
     bar.visible = true;
 		}
     });
+	this.bars.visible = this._canViewMode(this.document.displayBars);
   }
 
 
 
-function _refreshBorder_Override() {
+function refreshHUD_Override({bars=true, border=true, effects=true, elevation=true, nameplate=true}={}) {
+    if ( bars ) this.drawBars();
+    if ( effects ) this._refreshEffects();
+    if ( elevation ) {
+      const tt = this._getTooltipText();
+      if ( tt !== this.tooltip.text ) this.tooltip.text = tt;
+      this.tooltip.position.set(this.w / 2, -2);
+    }
+    if ( nameplate ) {
+      if ( this.document.name !== this.nameplate.text ) this.nameplate.text = this.document.name;
+      this.nameplate.position.set(this.w / 2, this.h + 2);
+      this.nameplate.visible = this._canViewMode(this.document.displayName);
+    }
+	if ( border ) { // this is the overridden part for various parameters
     this.border.clear();
+    this.border.position.set(this.document.x, this.document.y);
+    if ( !this.visible ) return;		
     const borderColor = this._getBorderColor();
     if( !borderColor ) return;
-
 	const frameScale = Math.round(game.settings.get(MODULE_ID, 'frame-scale')); 
 	const frameAlpha = (game.settings.get(MODULE_ID, 'frame-alpha'));
-
 	const frameBlkBorderAlpha = (game.settings.get(MODULE_ID, 'frameblk-border-alpha'));
 	const borderFillSelected = (game.settings.get(MODULE_ID, 'border-fill-selected')); 
 	const borderFillHover = (game.settings.get(MODULE_ID, 'border-fill-hover'));
-
 	const t = Math.round(CONFIG.Canvas.objectBorderThickness * frameScale);
 	const frameMargin = (game.settings.get(MODULE_ID, 'frame-margin'));
-
     
     // Draw Hex border for size 1 tokens on a hex grid
+    if ( canvas.grid.isHex ) {
+		
     const gt = CONST.GRID_TYPES;
     const hexTypes = [gt.HEXEVENQ, gt.HEXEVENR, gt.HEXODDQ, gt.HEXODDR];
-    if ( hexTypes.includes(canvas.grid.type) && (this.data.width === 1) && (this.data.height === 1) ) {
+    if ( hexTypes.includes(canvas.grid.type) && (this.document.width === 1) && (this.document.height === 1) ) {
       const polygon = canvas.grid.grid.getPolygon(-1+frameMargin, -1+frameMargin, this.w+2-2*frameMargin, this.h+2-2*frameMargin);
 
     this.border.lineStyle(t, 0x000000, frameBlkBorderAlpha).drawPolygon(polygon);
 
-    if (this._controlled) {
+    if (this.controlled) {
       this.border.beginFill(borderColor, borderFillSelected).lineStyle(t/2, borderColor, frameAlpha).drawPolygon(polygon);
       } else {
       this.border.beginFill(borderColor, borderFillHover).lineStyle(t/2, borderColor, frameAlpha).drawPolygon(polygon);
       }
     }
+	}
 
     // Otherwise Draw Square border
     else {
@@ -116,25 +135,25 @@ function _refreshBorder_Override() {
       const o = Math.round( -(h/2) + frameMargin);
 
 	this.border.lineStyle(t, 0x000000, frameBlkBorderAlpha).drawRoundedRect(o, o, (this.w+h)-2*frameMargin, (this.h+h)-2*frameMargin, 3);
-      if (this._controlled) {
+      if (this.controlled) {
 	this.border.beginFill(borderColor, borderFillSelected).lineStyle(h, borderColor, frameAlpha).drawRoundedRect(o, o, (this.w+h)-2*frameMargin, (this.h+h)-2*frameMargin, 3);
       } else {
         this.border.beginFill(borderColor, borderFillHover).lineStyle(h, borderColor, frameAlpha).drawRoundedRect(o, o, (this.w+h)-2*frameMargin, (this.h+h)-2*frameMargin, 3);
       }
 
     }
-  }
+	}
+}
 
 
+function _getBorderColor_Override({hover}={}) {
 
-function _getBorderColor_Override() {
-
-    const gmDisposition = (game.settings.get(MODULE_ID, 'gm-disposition'));
- 
+	const gmDisposition = (game.settings.get(MODULE_ID, 'gm-disposition'));
     const colors = CONFIG.Canvas.dispositionColors;
-      if ( this._controlled && !gmDisposition ) return colors.CONTROLLED;
-      else if ( this._hover || this._controlled) {
-         let d = parseInt(this.data.disposition);  // change for disposition color option
+	
+      if ( this.controlled && !gmDisposition ) return colors.CONTROLLED;	  
+      else if ( (this.controlled && gmDisposition) || (hover ?? this.hover) || canvas.tokens._highlight ) {
+         let d = parseInt(this.document.disposition);  // change for disposition color option
          if (!game.user.isGM && this.isOwner) return colors.CONTROLLED;
          else if (this.actor?.hasPlayerOwner) return colors.PARTY;
          else if (d === CONST.TOKEN_DISPOSITIONS.FRIENDLY) return colors.FRIENDLY;
@@ -145,7 +164,6 @@ function _getBorderColor_Override() {
   }
 
 
-  
 Hooks.once('init', function () {
 
 	new window.Ardittristan.ColorSetting(MODULE_ID, 'bar2empty', {
@@ -359,11 +377,11 @@ Hooks.once('setup', function () {
 
     libWrapper.register(MODULE_ID, 'Token.prototype.drawBars', drawBars_Override, "OVERRIDE");
 
-    libWrapper.register(MODULE_ID, 'Token.prototype._refreshBorder', _refreshBorder_Override, "OVERRIDE");
+    libWrapper.register(MODULE_ID, 'Token.prototype.refreshHUD', refreshHUD_Override, "OVERRIDE");
 
     libWrapper.register(MODULE_ID, 'Token.prototype._getBorderColor', _getBorderColor_Override, "OVERRIDE");
 
-    console.log(`Attribute Bar Colors v1.0 | initialized`);
+    console.log(`Attribute Bar Colors v2.0 | initialized`);
 })
 
 
@@ -371,4 +389,4 @@ Hooks.once('ready', () => {
     try{window.Ardittristan.ColorSetting.tester} catch {
         ui.notifications.notify('Please make sure you have the "lib - ColorSettings" module installed and enabled.', "error");
     }
-});
+})
